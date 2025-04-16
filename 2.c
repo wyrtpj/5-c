3245c5af7b5efd066fac8e9621e06315d79a8448
маш маш маш <mtnkuv@mail.ru>
2025-04-16 08:26:40 +0300

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() 
+{
+    char word[] = "yellow";
+    char result[4]; 
+    int start = 3; 
+    int end = 6; 
+
+    int j = 0; 
+    for (int i = start; i < end; i++) 
+    {
+        result[j] = word[i];
+        j++;
+    }
+    result[j] = '\0'; 
+
+    printf("Итог: %s\n", result);
+
+    return 0;
+}
